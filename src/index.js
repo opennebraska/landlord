0e8796e4245870780e68cecbf4bcc07c15359f40
erikdodge <edodge-chwy>
2020-08-12 21:50:37 -0500

    Adding basic app bar and removing CRA default page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { FirebaseDatabaseProvider } from "@react-firebase/database";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseDatabaseProvider>
+      <App />
+    </FirebaseDatabaseProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
