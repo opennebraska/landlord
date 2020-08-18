c3ea96b7edb0dc642b2b412d3a77202bbc688266
erikdodge <edodge-chwy>
2020-08-17 22:27:38 -0500

    Adding routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
 import "./App.css";
 import MainAppBar from "./MainAppBar";
+import ParcelTable from "./ParcelTable";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./Home";
 
 function App() {
-  return (
-    <React.Fragment>
-      <MainAppBar />
-      <div>Body</div>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <MainAppBar />
+            <Router>
+                <Switch>
+                    <Route path="/out-of-state">
+                        <ParcelTable />
+                    </Route>
+                    <Route path="/out-of-omaha">
+                        <ParcelTable />
+                    </Route>
+                    <Route path="/poor-condition">
+                        <ParcelTable />
+                    </Route>
+                    <Route path="/" exact={true}>
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+        </React.Fragment>
+    );
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Home() {
+    return <div>This is the home page</div>
+}
