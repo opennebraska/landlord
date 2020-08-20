1a71cd1227be09f7c00f7a4e13dbb2abf61f3d07
erikdodge <edodge-chwy>
2020-08-20 13:12:12 -0500

    adding basename to resolve gh-pages issue.

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -8,7 +8,7 @@ import {lowConditionTableData} from "./LowConditionTableData";
 
 export default function MainRouter() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/out-of-state">
           <ParcelTable tableData={outOfStateTableData} />
