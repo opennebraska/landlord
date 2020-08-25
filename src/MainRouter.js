3debb60123d505d69c62efd43c06b852e1805efa
erikdodge <edodge-chwy>
2020-08-25 00:22:41 -0500

    BASE_PATH for basename

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -8,7 +8,7 @@ import MainAppBar from "./MainAppBar";
 
 export default function MainRouter() {
     return (
-        <Router basename={"landlord"}>
+        <Router basename={process.env.BASE_PATH}>
             <Switch>
                 <Route path="/out-of-state">
                     <MainAppBar>
