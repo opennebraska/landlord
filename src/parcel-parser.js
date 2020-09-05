b812a850ce4a33016539b4984a1296e95acda551
erikdodge <edodge-chwy>
2020-09-05 08:29:55 -0600

    Fix low condition data to not include properties with the same address as the owner.

--- src/parcel-parser.js
+++ src/parcel-parser.js
@@ -6,14 +6,30 @@ console.log(`End Time Import: ${new Date().toString()}`)
 console.log(`Start Loop Time: ${new Date().toString()}`)
 let totalNE = 0
 
-let data = undefined
-for(let i = 0; i< parcels.length-1; i++){
-    if(parcels[i].OWNER_STAT === 'NE'){
-        totalNE+=1;
-    }
+let data = []
+
+const isOwnersProperty = (parcel) => {
+    const propertyAddress = parcel.ADDRESS_LA;
+    const ownerAddress = parcel.ADDRESS2;
+    return propertyAddress === ownerAddress;
 }
+
+const includeInLowCondition = (parcel) => {
+    const lowConditions = ["LOW", "WORN OUT", "POOR"];
+    const parcelCondition = parcel["CONDITION"];
+    const hasLowCondition = lowConditions.includes(parcelCondition.toUpperCase());
+    const notOwnersProperty = !isOwnersProperty(parcel);
+    return hasLowCondition && notOwnersProperty;
+}
+
+parcels.forEach((parcel) => {
+    if(includeInLowCondition(parcel)){
+        data.push(parcel);
+    }
+})
+
 const fileName = `parser-results-${new Date().getTime()}.json`
-fs.writeFileSync(fileName, data, function (err) {
+fs.writeFileSync(fileName, JSON.stringify(data), function (err) {
     if (err) return console.log(err);
     console.log(`Data in ${fileName}`);
   })
