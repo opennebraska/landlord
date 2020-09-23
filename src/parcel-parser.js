1af374860ec0ce6d8684e1f9817e46936bbc10c0
Jay Hannah <jay.hannah@iinteractive.com>
2020-09-22 20:56:03 -0500

    Fixes #28: OMAHA properties only

--- src/parcel-parser.js
+++ src/parcel-parser.js
@@ -8,20 +8,24 @@ let totalNE = 0
 
 let data = []
 
+// used by includeInLowCondition below
 const isOwnersProperty = (parcel) => {
     const propertyAddress = parcel.ADDRESS_LA;
     const ownerAddress = parcel.ADDRESS2;
-    return propertyAddress === ownerAddress;
+    return propertyAddress === ownerAddress && propertyInsideOmaha;
 }
 
+// low_condition_properties.json
 const includeInLowCondition = (parcel) => {
     const lowConditions = ["LOW", "WORN OUT", "POOR"];
     const parcelCondition = parcel["CONDITION"];
     const hasLowCondition = lowConditions.includes(parcelCondition.toUpperCase());
     const notOwnersProperty = !isOwnersProperty(parcel);
-    return hasLowCondition && notOwnersProperty;
+    const propertyInsideOmaha = parcel.PROP_CITY.toUpperCase() === "OMAHA";
+    return hasLowCondition && notOwnersProperty && propertyInsideOmaha;
 }
 
+// owner_in_state_out_of_city.json
 const includeInOutOfOmaha = (parcel) => {
     const ownerOutsideOmaha = parcel.OWNER_CITY.toUpperCase() !== "OMAHA";
     const ownerFromNebraska = parcel.OWNER_STAT.toUpperCase() === "NE";
@@ -29,8 +33,15 @@ const includeInOutOfOmaha = (parcel) => {
     return ownerOutsideOmaha && ownerFromNebraska && propertyInsideOmaha;
 }
 
+// owner_out_of_state.json
+const ownerOutOfState = (parcel) => {
+    const ownerFromNebraska = parcel.OWNER_STAT.toUpperCase() === "NE";
+    const propertyInsideOmaha = parcel.PROP_CITY.toUpperCase() === "OMAHA";
+    return !ownerFromNebraska && propertyInsideOmaha;
+}
+
 parcels.forEach((parcel) => {
-    if(includeInOutOfOmaha(parcel)){
+    if(ownerOutOfState(parcel)){
         data.push(parcel);
     }
 })
