c26b1eee161ee2c8fc6c14addd7a8485649088cc
Ron Watts <ronaldewatts@gmail.com>
2020-10-25 08:05:35 -0500

    #76 code review comments * remove re-init'ing GA * clean up ternary for `CONDITION` and `QUALITY`

--- src/PropertyDetail.js
+++ src/PropertyDetail.js
@@ -20,7 +20,7 @@ export default function PropertyDetail() {
 
   const {source, pin} = useParams();
   useEffect(() => {
-    ReactGA.initialize('UA-175185008-1');
+    ReactGA.pageview(window.location.pathname);
     let parcelData;
     if (source === "out-of-state") {
       parcelData = outOfNebraskaParcelsData;
@@ -59,8 +59,8 @@ export default function PropertyDetail() {
               <Typography variant={"body1"} component={"p"}>{selectedProperty.PROP_CITY}, NE {selectedProperty.PROP_ZIP}</Typography>
               <Typography variant={"body1"} component={"p"} gutterBottom><Link target="_blank" rel="noreferrer" href={`http://maps.google.com/?q=${encodedPropertyAddress}`}>View On Map</Link></Typography>
               <Typography variant={"h6"} gutterBottom>Condition/Quality</Typography>
-              <Typography variant={"body1"} component={"p"}>Condition: {selectedProperty.CONDITION.trim() === "" ? 'Unknown' : selectedProperty.CONDITION}</Typography>
-              <Typography variant={"body1"} component={"p"} gutterBottom>Quality: {selectedProperty.QUALITY.trim() === "" ? 'Unknown' : selectedProperty.QUALITY}</Typography>
+              <Typography variant={"body1"} component={"p"}>Condition: {selectedProperty.CONDITION.trim() || "Unknown"}</Typography>
+              <Typography variant={"body1"} component={"p"} gutterBottom>Quality: {selectedProperty.QUALITY.trim() || "Unknown"}</Typography>
               <Typography variant={"h6"} gutterBottom>Case Links:</Typography>
               {violationLinks.length > 0 && violationLinks.map(link => <Typography variant={"body"} component={"p"} gutterBottom><Link target="_blank" rel="noreferrer" href={`${link}`}>View Case</Link></Typography>)}
               {violationLinks.length === 0 && <Typography variant={"body1"} component={"p"}>No Cases</Typography>}
