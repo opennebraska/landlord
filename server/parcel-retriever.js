7bc2e15a3a0e21d002dd2f2329936937508f6a20
Ron Watts <ronaldewatts@gmail.com>
2021-03-06 08:53:21 -0600

    log urls used for fetching data

--- server/parcel-retriever.js
+++ server/parcel-retriever.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 
-const FIELDS = 'OBJECTID,PIN,OWNER_NAME,ADDRESS1,ADDRESS2,OWNER_CITY,OWNER_STAT,OWNER_ZIP,PROPERTY_A,HOUSE,APARTMENT,PROP_CITY,PROP_ZIP,BLOCK,LOT,QUALITY,CONDITION,ADDRESS_LA,X_COORD,Y_COORD';
+const FIELDS = "OBJECTID,PIN,OWNER_NAME,ADDRESS1,ADDRESS2,OWNER_CITY,OWNER_STAT,OWNER_ZIP,PROPERTY_A,HOUSE,APARTMENT,PROP_CITY,PROP_ZIP,BLOCK,LOT,QUALITY,CONDITION,ADDRESS_LA,X_COORD,Y_COORD";
 const WHERE = "UPPER(CONDITION)+IN+('POOR','LOW','WORN+OUT')+OR+(UPPER(OWNER_CITY)<>'OMAHA'+AND+UPPER(OWNER_STAT)='NE'+AND+UPPER(PROP_CITY)='OMAHA')+OR+UPPER(OWNER_STAT)<>'NE'";
 const PAGE_SIZE_DEFAULT = 1000;
 
 async function getParcelsCount() {
-    const parcelsCountResponse = await axios.get(`https://gis.dogis.org/arcgis/rest/services/OpenData_Layers/MapServer/38/query?where=${WHERE}&returnCountOnly=true&f=json`);
+    const url = `https://gis.dogis.org/arcgis/rest/services/OpenData_Layers/MapServer/38/query?where=${WHERE}&returnCountOnly=true&f=json`;
+    console.log(`Fetching data count from ${url}`);
+    const parcelsCountResponse = await axios.get(url);
     // TODO need to make sure there is data and count
     return parcelsCountResponse.data.count;
 }
@@ -13,6 +15,7 @@ const getParcelsUrl = (resultOffset, resultRecordCount) => `https://gis.dogis.or
 
 async function getParcels(page, pageSize) {
     const pageUrl = getParcelsUrl(page, pageSize);
+    console.log(`Fetching data from  ${pageUrl}`);
     const parcelsFeaturesResponse = await axios.get(pageUrl);
     const parcelsFeatures = parcelsFeaturesResponse.data;
     return parcelsFeatures.features;
