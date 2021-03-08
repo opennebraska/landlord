e153e42a12a19d0cf0c35aef79b923f1c09c7850
dodgees <dodgees2@gmail.com>
2021-03-07 20:39:12 -0600

    Adding some initial meta data information

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactGA from 'react-ga';
 import "./App.css";
 import MainRouter from "./MainRouter";
+import Helmet from "react-helmet";
 
 ReactGA.initialize('UA-175185008-1');
 
@@ -9,6 +10,10 @@ function App() {
 
     return (
         <React.Fragment>
+            <Helmet>
+              <title>Landlords of Omaha</title>
+              <meta name="description" content="Property information and housing code violations for rental properties in Omaha and Douglas County"/>
+            </Helmet>
             <MainRouter/>
         </React.Fragment>
     );
